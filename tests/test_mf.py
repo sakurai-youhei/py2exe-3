e0aaab9200d73f418edcc29e0ba7b0bcf7231ed0
Youhei Sakurai <sakurai.youhei@gmail.com>
2016-09-25 15:18:41 +0900

    Remove errors in tests

--- tests/test_mf.py
+++ tests/test_mf.py
@@ -2,8 +2,34 @@
 import unittest
 
 from py2exe.mf3 import ModuleFinder
+from tests.test_make import _TestPackageBase, Test_PEP328 as _PEP328
 
-class MFTest(unittest.TestCase):
+class MFTest(_TestPackageBase):
+    data = """
+    testmods/test_tools.py
+            from testmods.tools import bar
+            from testmods.tools import baz
+            from testmods.tools import spam
+            from testmods.tools import foo
+            try: from testmods.tools import spam_and_eggs
+            except ImportError: pass
+
+    testmods/__init__.py
+            # empty
+
+    testmods/tools/spamfoo.py
+            spam = 'spam'
+            foo = 'foo'
+
+    testmods/tools/bazbar.py
+            baz = 'baz'
+            bar = 'bar'
+
+    testmods/tools/__init__.py
+            from .bazbar import *
+            from .spamfoo import spam
+            from .spamfoo import foo
+    """
 
     # c:/python33/lib/collections/
     def test_collections(self):
@@ -23,28 +49,7 @@ class MFTest(unittest.TestCase):
                   if name.startswith("collections")]
         self.assertEqual([], missed)
 
-    def test_pep328(self):
-        """
-        The package structure from pep328.
-        """
-
-        mf = ModuleFinder()
-        mf.import_hook("pep328.subpackage1")
-        self.assertEqual(set(mf.modules.keys()),
-                         {"pep328",
-                          "pep328.moduleA",
-                          "pep328.subpackage1",
-                          "pep328.subpackage1.moduleX",
-                          "pep328.subpackage1.moduleY",
-                          "pep328.subpackage2",
-                          "pep328.subpackage2.moduleZ",
-                          })
-        self.assertFalse(mf.missing())
-
-
     # /python33/lib/site-packages/nose/tools/nontrivial.py
-
-
     def test_tools(self):
         """
         check that these are not reported as missing modules:
@@ -54,33 +59,8 @@ class MFTest(unittest.TestCase):
         - testmods.tools.spam
         - testmods.tools.foo
         """
-        
-        src = """
-        The package structure is like this:
-        
-        testmods/test_tools.py:
-            import testmods.tools.bar
-            import testmods.tools.baz
-            import testmods.tools.spam
-            import testmods.tools.foo
 
-        testmods/tools/spamfoo.py:
-            spam = "spam"
-            bar = "bar"
-
-        testmods/tools/bazbar.py
-            baz = "baz"
-            bar = "bar"
-
-        testmods/tools/__init__.py
-            from .bazbar import *
-
-        testmods/tools/__init__.py
-            from .spamfoo import spam, foo
-
-        """
-
-        mf = ModuleFinder(debug=debug)
+        mf = ModuleFinder(verbose=2)
         mf.import_hook("testmods.test_tools")
         self.assertEqual(set(mf.modules.keys()), {
             "testmods",
@@ -88,6 +68,25 @@ class MFTest(unittest.TestCase):
             "testmods.tools",
             "testmods.tools.bazbar",
             "testmods.tools.spamfoo"})
+        self.assertFalse(mf.missing())
+
+class Test_PEP328(_PEP328):
+    def test_pep328(self):
+        """
+        The package structure from pep328.
+        """
+
+        mf = ModuleFinder()
+        mf.import_hook("pep328.sub1")
+        self.assertEqual(set(mf.modules.keys()),
+                         {"pep328",
+                          "pep328.moduleA",
+                          "pep328.subpackage1",
+                          "pep328.subpackage1.moduleX",
+                          "pep328.subpackage1.moduleY",
+                          "pep328.subpackage2",
+                          "pep328.subpackage2.moduleZ",
+                          })
         self.assertFalse(mf.missing())
 
 if __name__ == "__main__":
